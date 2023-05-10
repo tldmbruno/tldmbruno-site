e4991c933c8b8aad2f9feb6a9b7e5d49a7a5e8cd
Bruno Peres <dev.brunoperes@gmail.com>
2023-05-10 04:04:21 -0300

    Add "SITE UNDER CONSTRUCTION" warning

--- src/components/ItsDangerousToGoAlone/ItsDangerousToGoAlone.tsx
+++ src/components/ItsDangerousToGoAlone/ItsDangerousToGoAlone.tsx
@@ -6,7 +6,7 @@ function ItsDangerousToGoAlone() {
       <h1 className={style.oldManSpeech}>IT'S DANGEROUS TO GO ALONE. TAKE THIS.</h1>
       <img className={style.floatingSword} src={'sword.png'} />
       <p>
-        <code>@tldmbruno on GitHub</code>
+        <code>SITE UNDER CONSTRUCTION | @tldmbruno on GitHub | @thelastdarkmage on itch.io</code>
       </p>
     </>
   );
