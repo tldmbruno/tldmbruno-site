3272d2cc9d25fa141a3545c89165c9b32dd2bb94
Bruno Peres <dev.brunoperes@gmail.com>
2023-05-10 04:06:24 -0300

    Add link at sword to itch.io page

--- src/components/ItsDangerousToGoAlone/ItsDangerousToGoAlone.tsx
+++ src/components/ItsDangerousToGoAlone/ItsDangerousToGoAlone.tsx
@@ -4,7 +4,9 @@ function ItsDangerousToGoAlone() {
   return (
     <>
       <h1 className={style.oldManSpeech}>IT'S DANGEROUS TO GO ALONE. TAKE THIS.</h1>
-      <img className={style.floatingSword} src={'sword.png'} />
+      <a href='https://thelastdarkmage.itch.io/'>
+        <img className={style.floatingSword} src={'sword.png'}/>
+      </a>
       <p>
         <code>SITE UNDER CONSTRUCTION | @tldmbruno on GitHub | @thelastdarkmage on itch.io</code>
       </p>
